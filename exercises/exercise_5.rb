20b2d9eb1002e30d3d3ca63e3284b07ebb1a0d79
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-04-06 19:06:09 -0400

    calculate total revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@sum = Store.sum("annual_revenue")
+puts "Sum of all annual revenue: #{@sum}"
+@count = Store.all.count
+@average = @sum/@count
+puts "Average annual revenue between all #{@count} stores: $#{@average}"
+
+puts "There are #{Store.where("annual_revenue >= 1000000").all.count} stores that makes one million or more annually"
