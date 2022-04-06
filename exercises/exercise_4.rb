4eb567f9551ec6ac24f2e6543dc498d1dc8ae391
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-04-06 18:50:25 -0400

    load a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,21 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: false, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: false)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+store_num = @mens_stores.all.count
+puts "There are #{store_num} stores that's carrying mens apparel"
+
+@mens_stores.all.map {|store| puts "Store in #{store.name} makes around #{store.annual_revenue} annually" }
+
+@womens_stores = Store.where("womens_apparel = true and annual_revenue < 1000000")
+
+store_num = @womens_stores.all.count
+puts "There are #{store_num} stores that's carrying womens apparel"
+
+@womens_stores.all.map {|store| puts "Store that carries women cloth in #{store.name} makes around #{store.annual_revenue} annually"}
