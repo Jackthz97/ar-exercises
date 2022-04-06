9c012cce27c8236601470ff982998f4763c79b20
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-04-06 19:17:02 -0400

    Add more employees to the employees database

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 15)
+@store1.employees.create(first_name: "Mike", last_name: "Maylack", hourly_rate: 21)
+@store1.employees.create(first_name: "James", last_name: "Brown", hourly_rate: 30)
+@store1.employees.create(first_name: "Sean", last_name: "Banana", hourly_rate: 60)
+@store1.employees.create(first_name: "Calvin", last_name: "Xu", hourly_rate: 60)
+@store2.employees.create(first_name: "Jack", last_name: "Zhang", hourly_rate: 200)
+@store2.employees.create(first_name: "Charles", last_name: "Zhang", hourly_rate: 400)
+@store2.employees.create(first_name: "Saatvik", last_name: "Nana", hourly_rate: 200)
